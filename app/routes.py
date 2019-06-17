fb79df492b031e3ca2f6bf64dcfa00e6d4578196
oonikkoo <thanabadeepunyanitya@gmial.com>
2019-06-18 02:40:45 +0700

    kuy

--- app/routes.py
+++ app/routes.py
@@ -215,4 +215,7 @@ def add_header(r):
     r.headers["Pragma"] = "no-cache"
     r.headers["Expires"] = "0"
     r.headers['Cache-Control'] = 'public, max-age=0'
+
     return r
+
+# yttttttttttttttttttttttttttttt
